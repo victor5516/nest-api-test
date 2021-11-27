31b0dd6f57c0b78d96fcd6abcac98b32a3788af1
Victor5516 <Victor5516@gmail.com>
2021-11-27 18:56:32 -0400

    fix service test

--- src/admins/test/admins.service.spec.ts
+++ src/admins/test/admins.service.spec.ts
@@ -1,62 +1,64 @@
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
+import { Connection, createConnection } from 'mongoose';
 import { AdminsService } from '../admins.service';
-import { Status } from '../models/status';
-
 import { Admin } from '../shchemas/admin.schema';
 import { adminStub, adminStubs } from './stubs/admin.stubs';
+import { UpdateAdminDto } from './../dto/update-admin.dto';
+import { CreateAdminDto } from './../dto/create-admin.dto';
 
 
-const adminMock = {
-id: '61a0357c3fcb762b9d80f45c',
-name:'Alberto',
-lastName:'Suarez',
-email:'newemail@email.com',
-role:'admin.fleetrw',
-status:Status.Active,
-picture:'fine.com'}
-
+class AdminsServiceMock {
+  create(dto: CreateAdminDto) {
+    return adminStub();
+  }
+  findAll() {
+    return adminStubs();
+  }
+  remove(id: string) {
+    return adminStub();
+  }
+  update(id: string, dto: UpdateAdminDto) {
+    return adminStub();
+  }
+}
 describe('AdminsService', () => {
   let adminService: AdminsService;
   let adminModel: Model<Admin>;
-
+  const ApiServiceProvider = {
+    provide: AdminsService,
+    useClass: AdminsServiceMock,
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule,],
-      providers: [AdminsService,
+      imports: [HttpModule],
+      providers: [
+        AdminsService,
+        ApiServiceProvider,
         {
-        provide: getModelToken(Admin.name),
-        useValue: {
-          new: jest.fn().mockResolvedValue(adminStub()),
-          constructor: jest.fn().mockResolvedValue(adminStub()),
-          find: jest.fn(),
-          findOne: jest.fn(),
-          findById:jest.fn(),
-          create: jest.fn(),
-          findByIdAndUpdate: jest.fn(),
-          findByIdAndDelete: jest.fn(),
-          where:jest.fn(),
-          exec: jest.fn(),
+          provide: getModelToken(Admin.name),
+          useValue: {
+            find: jest.fn(),
+            create: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndDelete: jest.fn(),
+          },
         },
-      },
-    ],
-
+      ],
     }).compile();
-
-
 
     adminService = module.get<AdminsService>(AdminsService);
     adminModel = module.get<Model<Admin>>(getModelToken(Admin.name));
-
   });
+
 
   it('should be defined', () => {
     expect(adminService).toBeDefined();
   });
 
-  it('should return all admins', async () => {
+  it('should call findAll method with expected params', async () => {
     jest.spyOn(adminModel, 'find').mockReturnValue({
       exec: jest.fn().mockResolvedValueOnce(adminStubs()),
     } as any);
@@ -65,23 +67,31 @@ describe('AdminsService', () => {
     expect(admins).toEqual(adminStubs());
   });
 
-  it('should insert a new admin', async () => {
-    jest.spyOn(adminModel, 'create').mockImplementationOnce(() =>
-      Promise.resolve(adminStub()),
-    );
+  it('should call create method with expected params', async () => {
+    jest
+      .spyOn(adminModel, 'create')
+      .mockImplementationOnce(() => Promise.resolve(adminStub()));
     const newAdmin = await adminService.create(adminStub());
-     expect(newAdmin).toEqual(adminStub());
+    expect(newAdmin).toEqual(adminStub());
   });
 
-  it('should update a  admin', async () => {
+  it('should call update method with expected params', async () => {
     const id = '61a0357c3fcb762b9d80f45c';
     jest.spyOn(adminModel, 'findByIdAndUpdate').mockReturnValue({
       exec: jest.fn().mockResolvedValueOnce(adminStub()),
     } as any);
-    const newAdmin = await adminService.update(id,adminStub());
-     expect(newAdmin).toEqual(adminStub());
+    const dto = new UpdateAdminDto();
+    const admin = await adminService.update(id, dto);
+    expect(admin).toEqual(adminStub());
   });
 
+  it('should call remove method with expected params', async () => {
+    const id = '61a0357c3fcb762b9d80f45c';
+    jest.spyOn(adminModel, 'findByIdAndDelete').mockReturnValue({
+      exec: jest.fn().mockResolvedValueOnce(adminStub()),
+    } as any);
 
-})
-
+    const admin = await adminService.remove(id);
+    expect(admin).toEqual(adminStub());
+  });
+});
